db3b9a0876dc85c501fb68cae09b6ffe3176725e
Fabian Fleck <faabianfleeck@gmail.com>
2024-04-20 23:46:44 -0300

    Initial commit

---
+++ src/main/java/br/com/onitama/OnitamaApplication.java
@@ -0,0 +1,12 @@
+package br.com.onitama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnitamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnitamaApplication.class, args);
+    }
+}
---
+++ src/test/java/br/com/onitama/OnitamaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.onitama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnitamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
